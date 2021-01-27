33efb2936cca9c331a87716d5c029695413f944d
Homka <homakz93@gmail.com>
2021-01-28 00:10:55 +0200

    Added static files

--- MyApp/mainapp/templates/main/index.js
+++
@@ -1,9 +0,0 @@
-
-function AgePlusThree(age) {
-    return (Number(age) + 3);
-}
-
-let Age = prompt('Your age?','10')
-
-alert(`Через 3 года тебе будет ${AgePlusThree(Age)}`)
-
--- MyApp/mainapp/views.py
+++ MyApp/mainapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, HttpResponse 
-
+from .models import *
 
 # Create your views here.
 def index(request):
     return render(request, template_name='main/index.html')
+    
+def articles(request):
+
+    return render(request, template_name='main/articles.html', context={'content':Posts.objects.all()})
